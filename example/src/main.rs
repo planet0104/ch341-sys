26f27be9f866ca18727ed5c75bbc82238257c087
JiaYe <planet2@qq.com>
2024-04-01 09:15:59 +0800

    Update main.rs

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,4 +1,4 @@
-use std::{ffi::{c_char, CStr}, time::Duration};
+use std::time::Duration;
 
 use ch341::{CH341CloseDevice, CH341OpenDevice, CH341SetOutput};
 use ch341_sys as ch341;
@@ -7,10 +7,12 @@ fn main() {
     unsafe{
         //https://github.com/tomek-o/CH341A-tool
         blink();
+        st7789();
     }
     
 }
 
+#[allow(unused)]
 unsafe fn blink(){
     let dev = CH341OpenDevice(0);
     
@@ -20,29 +22,38 @@ unsafe fn blink(){
     }
     println!("设备打开成功");
 
-    // 位7-位0对应CH341的D7-D0引脚
-
     // 位2为1说明iSetDataOut的7-位0有效,否则忽略
     // 位3为1说明iSetDirOut的位7-位0有效,否则忽略
-    let enable = 1<<2;
+    let enable = 0b1100;
     
-    //iSetDirOut  设置I/O方向,某位清0则对应引脚为输入,某位置1则对应引脚为输出,并口方式下默认值为0x000FC000,参考下面的位说明
-	//iSetDataOut  输出数据,如果I/O方向为输出,那么某位清0时对应引脚输出低电平,某位置1时对应引脚输出高电平,参考下面的位说明
+    //iSetDirOut 低8位对应CH341的D7-D0引脚。
+	//iSetDataOut 低8位对应CH341的D7-D0引脚的电平输出。
+    for _ in 0..5{
+        let set_dir_out = 0b111;
+        let set_data_out = 0b101;
+        let _ = CH341SetOutput(0, enable, set_dir_out, set_data_out);
+        std::thread::sleep(Duration::from_millis(300));
 
-    for _ in 0..20{
-        let set_dir_out = 1; // 第0位为1，代表D0引脚为输出
-        let set_data_out = 1; // 第0位为1，代表D0引脚输出高电平
-        let success = CH341SetOutput(0, enable, set_dir_out, set_data_out);
-        println!("GPIO写入{success}");
-        std::thread::sleep(Duration::from_millis(1000));
-
-        let set_dir_out = 1; // 第0位为1，代表D0引脚为输出
-        let set_data_out = 0; // 第0位为1，代表D0引脚输出低电平
-        let success = CH341SetOutput(0, enable, set_dir_out, set_data_out);
-        println!("GPIO写入{success}");
-        std::thread::sleep(Duration::from_millis(1000));
+        let set_dir_out = 0b111;
+        let set_data_out = 0b010;
+        let _ = CH341SetOutput(0, enable, set_dir_out, set_data_out);
+        std::thread::sleep(Duration::from_millis(300));
     }
 
     CH341CloseDevice(0);
     println!("设备已关闭");
+}
+
+/*
+ST7789跟CH341A SPI连接方式
+VCC <=> VCC
+GND <=> GND
+SCL <=> SCK (时钟线)
+SDA <=> MISO (数据线)
+RES <=> 复位
+DC  <=> ？数据/命令选择
+BLK <=> VCC
+ */
+unsafe fn st7789(){
+
 }
